2a5f7b77d5f2bf0e8101df5610b7b166d3498eba
komisuomi <only-for-serzh@yandex.ru>
2020-10-17 11:20:03 +0300

    Updated Tests

--- src/test/java/ru/netology/manager/MovieManagerTest.java
+++ src/test/java/ru/netology/manager/MovieManagerTest.java
@@ -6,20 +6,22 @@ import ru.netology.domain.MovieItem;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MovieManagerTest {
+    MovieManager manager = new MovieManager();
+
+    MovieItem first = new MovieItem(1, 1, "Бладшот", "боевик");
+    MovieItem second = new MovieItem(2, 2, "Вперед", "мультфильм");
+    MovieItem third = new MovieItem(3, 3, "Отель Белград","комедия");
+    MovieItem fourth = new MovieItem(4, 4, "Джентльмены","боевик");
+    MovieItem fifth = new MovieItem(5, 5, "Человек-Невидимка","ужасы");
+    MovieItem sixth = new MovieItem(6, 6, "Тролли. Мировой тур","мультфильм");
+    MovieItem seventh = new MovieItem(7, 7, "Номер один","комедия");
+    MovieItem eighth = new MovieItem(8, 8, "Берегись авто","комедия");
+    MovieItem nineth = new MovieItem(9, 9, "Гадкий я","мультфильм");
+    MovieItem tenth = new MovieItem(10, 10, "Перестрелка","боевик");
+
     @Test
     public void shouldAddMovie() {
-        MovieManager manager = new MovieManager();
 
-        MovieItem first = new MovieItem(1, 1, "Бладшот", "боевик");
-        MovieItem second = new MovieItem(2, 2, "Вперед", "мультфильм");
-        MovieItem third = new MovieItem(3, 3, "Отель Белград","комедия");
-        MovieItem fourth = new MovieItem(4, 4, "Джентльмены","боевик");
-        MovieItem fifth = new MovieItem(5, 5, "Человек-Невидимка","ужасы");
-        MovieItem sixth = new MovieItem(6, 6, "Тролли. Мировой тур","мультфильм");
-        MovieItem seventh = new MovieItem(7, 7, "Номер один","комедия");
-        MovieItem eighth = new MovieItem(8, 8, "Берегись авто","комедия");
-        MovieItem nineth = new MovieItem(9, 9, "Гадкий я","мультфильм");
-        MovieItem tenth = new MovieItem(10, 10, "Перестрелка","боевик");
         manager.add(fifth);
 
         MovieItem[] actual = manager.getAll();
@@ -30,17 +32,7 @@ public class MovieManagerTest {
 
     @Test
     public void shouldShowLastAdded() {
-        MovieManager manager = new MovieManager();
-        MovieItem first = new MovieItem(1, 1, "Бладшот", "боевик");
-        MovieItem second = new MovieItem(2, 2, "Вперед", "мультфильм");
-        MovieItem third = new MovieItem(3, 3, "Отель Белград","комедия");
-        MovieItem fourth = new MovieItem(4, 4, "Джентльмены","боевик");
-        MovieItem fifth = new MovieItem(5, 5, "Человек-Невидимка","ужасы");
-        MovieItem sixth = new MovieItem(6, 6, "Тролли. Мировой тур","мультфильм");
-        MovieItem seventh = new MovieItem(7, 7, "Номер один","комедия");
-        MovieItem eighth = new MovieItem(8, 8, "Берегись авто","комедия");
-        MovieItem nineth = new MovieItem(9, 9, "Гадкий я","мультфильм");
-        MovieItem tenth = new MovieItem(10, 10, "Перестрелка","боевик");
+
         manager.add(first);
         manager.add(second);
         manager.add(third);
@@ -61,17 +53,6 @@ public class MovieManagerTest {
 
     @Test
     public void shouldShowLastAdded2() {
-        MovieManager manager = new MovieManager();
-        MovieItem first = new MovieItem(1, 1, "Бладшот", "боевик");
-        MovieItem second = new MovieItem(2, 2, "Вперед", "мультфильм");
-        MovieItem third = new MovieItem(3, 3, "Отель Белград","комедия");
-        MovieItem fourth = new MovieItem(4, 4, "Джентльмены","боевик");
-        MovieItem fifth = new MovieItem(5, 5, "Человек-Невидимка","ужасы");
-        MovieItem sixth = new MovieItem(6, 6, "Тролли. Мировой тур","мультфильм");
-        MovieItem seventh = new MovieItem(7, 7, "Номер один","комедия");
-        MovieItem eighth = new MovieItem(8, 8, "Берегись авто","комедия");
-        MovieItem nineth = new MovieItem(9, 9, "Гадкий я","мультфильм");
-        MovieItem tenth = new MovieItem(10, 10, "Перестрелка","боевик");
 
         manager.add(first);
         manager.add(second);
@@ -88,17 +69,7 @@ public class MovieManagerTest {
 
     @Test
     public void shouldShowLastAdded3() {
-        MovieManager manager = new MovieManager();
-        MovieItem first = new MovieItem(1, 1, "Бладшот", "боевик");
-        MovieItem second = new MovieItem(2, 2, "Вперед", "мультфильм");
-        MovieItem third = new MovieItem(3, 3, "Отель Белград","комедия");
-        MovieItem fourth = new MovieItem(4, 4, "Джентльмены","боевик");
-        MovieItem fifth = new MovieItem(5, 5, "Человек-Невидимка","ужасы");
-        MovieItem sixth = new MovieItem(6, 6, "Тролли. Мировой тур","мультфильм");
-        MovieItem seventh = new MovieItem(7, 7, "Номер один","комедия");
-        MovieItem eighth = new MovieItem(8, 8, "Берегись авто","комедия");
-        MovieItem nineth = new MovieItem(9, 9, "Гадкий я","мультфильм");
-        MovieItem tenth = new MovieItem(10, 10, "Перестрелка","боевик");
+
         manager.add(first);
 
         MovieItem[] actual = manager.getAll();
@@ -110,17 +81,7 @@ public class MovieManagerTest {
 
     @Test
     public void shouldShowLastAdded4() {
-        MovieManager manager = new MovieManager();
-        MovieItem first = new MovieItem(1, 1, "Бладшот", "боевик");
-        MovieItem second = new MovieItem(2, 2, "Вперед", "мультфильм");
-        MovieItem third = new MovieItem(3, 3, "Отель Белград","комедия");
-        MovieItem fourth = new MovieItem(4, 4, "Джентльмены","боевик");
-        MovieItem fifth = new MovieItem(5, 5, "Человек-Невидимка","ужасы");
-        MovieItem sixth = new MovieItem(6, 6, "Тролли. Мировой тур","мультфильм");
-        MovieItem seventh = new MovieItem(7, 7, "Номер один","комедия");
-        MovieItem eighth = new MovieItem(8, 8, "Берегись авто","комедия");
-        MovieItem nineth = new MovieItem(9, 9, "Гадкий я","мультфильм");
-        MovieItem tenth = new MovieItem(10, 10, "Перестрелка","боевик");
+
         manager.add(tenth);
 
         MovieItem[] actual = manager.getAll();
@@ -132,17 +93,7 @@ public class MovieManagerTest {
 
     @Test
     public void shouldShowLastAdded5() {
-        MovieManager manager = new MovieManager();
-        MovieItem first = new MovieItem(1, 1, "Бладшот", "боевик");
-        MovieItem second = new MovieItem(2, 2, "Вперед", "мультфильм");
-        MovieItem third = new MovieItem(3, 3, "Отель Белград","комедия");
-        MovieItem fourth = new MovieItem(4, 4, "Джентльмены","боевик");
-        MovieItem fifth = new MovieItem(5, 5, "Человек-Невидимка","ужасы");
-        MovieItem sixth = new MovieItem(6, 6, "Тролли. Мировой тур","мультфильм");
-        MovieItem seventh = new MovieItem(7, 7, "Номер один","комедия");
-        MovieItem eighth = new MovieItem(8, 8, "Берегись авто","комедия");
-        MovieItem nineth = new MovieItem(9, 9, "Гадкий я","мультфильм");
-        MovieItem tenth = new MovieItem(10, 10, "Перестрелка","боевик");
+
         manager.add(tenth);
         manager.add(first);
 
